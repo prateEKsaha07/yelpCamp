6881d7e00eea149ac701bf298b65d9195fc83992
prateEKsaha07 <155552003+prateEKsaha07@users.noreply.github.com>
2025-04-03 20:25:58 +0530

    initial repo

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+//middlewares
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
